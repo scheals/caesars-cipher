3c3f720434c8f07c3a5887650473a5c18d9dc9a7
scheals <piecoo@gmail.com>
2022-01-19 14:01:31 +0100

    Introduce actual solution

--- caesars_cipher.rb
+++ caesars_cipher.rb
@@ -1,11 +1,30 @@
 def caesar_cipher(string, shift_factor)
+  return string if shift_factor == 0
+  shift_factor += 26 if shift_factor < 0
   ciphered_string = ""
   broken_down_string = string.split(" ").map { |word| word.chars }
   shift_factor.times do
-    broken_down_string = broken_down_string.map { |word| word.map { |char| char.succ } }
+    broken_down_string = broken_down_string.map { |word| word.map { |character| caesar_succession(character) } }
     ciphered_string = broken_down_string.map { |word| word.join("") }.join(" ")
   end
   ciphered_string
 end
 
-p caesar_cipher("Try me out!", 2)
+def caesar_succession(character)
+  case character
+  when /\W/
+    return character
+  when "z"
+    return "a"
+  when "Z"
+    return "A"
+  else
+    return character.succ
+  end
+end
+
+p caesar_cipher("I am not interested in being changed.", 0)
+p caesar_cipher("I AM VERY LONG and complicated, what gives?!", 10)
+p caesar_cipher("I  FEAST  ON  ADDITIONAL  SPACES!", 1)
+p caesar_cipher("Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 1)
+p caesar_cipher(caesar_cipher("Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 3), -3)
