dc9ae75ba2319dc41aacce5bb22ac2e460c13fce
Scheals <piecoo@gmail.com>
2022-04-02 03:09:30 +0200

    Satisfy Rubocop with a small refactor

--- caesars_cipher.rb
+++ caesars_cipher.rb
@@ -1,6 +1,7 @@
 def caesar_cipher(string, shift_factor)
   if shift_factor <= 0
-    return string if shift_factor.remainder(26) == 0
+    return string if shift_factor.remainder(26).zero?
+
     shift_factor = shift_factor.remainder(26) + 26
   end
   shift_factor.times do
@@ -12,18 +13,18 @@ end
 def caesar_succession(character)
   case character
   when /\W/, /\s/
-    return character
-  when "z"
-    return "a"
-  when "Z"
-    return "A"
+    character
+  when 'z'
+    'a'
+  when 'Z'
+    'A'
   else
-    return character.succ
+    character.succ
   end
 end
 
-p caesar_cipher("I am not interested in being changed.", 0)
-p caesar_cipher("I AM VERY LONG and complicated, what gives?!", 10)
-p caesar_cipher("I  NO  LONGER  FEAST  ON  ADDITIONAL  SPACES!", 1)
-p caesar_cipher("Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 1)
-p caesar_cipher(caesar_cipher("Lorem ipsum dolor sit amet, consectetur adipiscing elit.", 3), -3)
+p caesar_cipher('I am not interested in being changed.', 0)
+p caesar_cipher('I AM VERY LONG and complicated, what gives?!', 10)
+p caesar_cipher('I  NO  LONGER  FEAST  ON  ADDITIONAL  SPACES!', 1)
+p caesar_cipher('Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 1)
+p caesar_cipher(caesar_cipher('Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 3), -3)
